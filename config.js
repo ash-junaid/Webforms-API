eb2242a0fcad93d979694e6f657e659ed9121452
Junaid Ashraf <ash.junaid29@gmail.com>
2026-02-05 18:37:33 +1100

    Update

--- config.js
+++ config.js
@@ -1,34 +1,18 @@
-
-
 // Instance tokens 
 window.tokens = {
-    percy: "PASTE_NEW_TOKEN_HERE",
+    percy: "25197baffb14d9fdbd881996bca965d8661a4af58325366844534a8c959bc89d7813ffebd6d7c8b0000116fe7afeabf045986db7615b0876391c3c03fdad689a4a6742e4e6e158d86f3e4f3f1f2efc12dd27c281c6517fbfc5bf34ea21449f54f44bff8702e215da4081142be15a95aad7740032d745300d49736a9fa17a7ca6b5d41846854a76f313664da4435bab9fb7414b88686e18287edcb1e46575c45a60be8be30d0235a138ac8ee902c914599ea6ec62affdee6cebc88e1e9f618576a32159a12b79765e09ba8a28866d4734cbe0636fcc74059b72af0f65a05191d61954ff0a8b20ed87a68ff641f53c8fbe9b68c02cb21359f7ad8784c0533e57395a6fa13442adddff9c9474d7019c7b5a",
     ron:   "PASTE_NEW_TOKEN_HERE",
     jo:    "PASTE_NEW_TOKEN_HERE"
 };
 
-// Web Form URL (Do not change)
+// pass variables
 const baseURL = "https://apps-d.docusign.com/webforms/us/ab4743a19e2f50ecb08d71eae9988706";
 
-
-//JS stuff
-
-function loadUser(userKey, btnElement) {
+function loadUser(key, btn) {
+    document.getElementById('welcome-text').style.display = 'none';
     
-    const welcomeText = document.getElementById('welcome-text');
-    if (welcomeText) welcomeText.style.display = 'none';
-    
-   
     document.querySelectorAll('button').forEach(b => b.classList.remove('active'));
-    btnElement.classList.add('active');
+    btn.classList.add('active');
 
- 
-    const token = window.tokens ? window.tokens[userKey] : null;
-
-    
-    if(token) {
-        document.getElementById('formFrame').src = baseURL + "#instanceToken=" + token;
-    } else {
-        alert("Error: Token not found for " + userKey + ". Please check config.js");
-    }
+    document.getElementById('formFrame').src = baseURL + "#instanceToken=" + window.tokens[key];
 }
