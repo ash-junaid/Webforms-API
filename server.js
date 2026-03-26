88e7b7ee7a4b34841ab0508dba610dda96d0ce56
Junaid Ashraf <ash.junaid29@gmail.com>
2026-03-26 16:47:35 +1100

    add comment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // IMPORT CONFIG
-const config = require('./config_server');
+const config = require('./config_server'); //change
 
 // HOST FRONTEND
 app.use(express.static(path.join(__dirname, 'public')));
